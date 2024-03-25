a313e7896f0608cffaa3ec24d4811e9b15744b30
Dani Flores <58187836+dfloresc@users.noreply.github.com>
2024-03-25 13:28:22 +0000

    Create pkg-publish.yml
    update setup.py
    
    change the long description for the readme file
    
    change name

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
 setup(
-    name="Process Orchestrator",
+    name="prorch",
     version="1.0.0",
     author="Dani Flores",
     author_email="dt.floresc@gmail.com",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
