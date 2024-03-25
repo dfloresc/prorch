f521720ce63ef777e76cd9b2fbeb33e86ba254ff
Daniel Flores Castillo <dt.floresc@gmail.com>
2024-03-25 13:43:50 +0000

    up to version 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="prorch",
-    version="1.0.0",
+    version="1.0.1",
     author="Dani Flores",
     author_email="dt.floresc@gmail.com",
     long_description=open('README.md').read(),
